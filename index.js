c7c3a6e04e31c72b1fc21e7c81f5eaaf55365a10
Anthony Ndegwa <anthon.n24@gmail.com>
2021-05-16 02:05:44 -0400

    restart2

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const mysql = require("mysql"); 
 const inquirer = require("inquirer");
-const { inherits } = require("util");
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
-const { connect } = require("http2");
-require("console.table"); 
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     //3306(standard),or personal port 
@@ -13,86 +9,69 @@ const connection = mysql.createConnection({
   // update your own sql password 
     password: "Thendgino9292",
  
-    database: "employee_db"
+    database: "employee_db",
 }); 
 
+/// co
 connection.connect((err) => { 
     if (err) throw err; 
-   console.log(`connected as id ${connection.threadId}\n`);
-   init(); 
+   console.log("connected as id" + connection.threadId)
+   startPrompt(); 
 });
- const stafflist = ['None']; 
- const DepList =  ['None'];
- const RoleList = [];
-// gathers all staff for manager array 
-const getStaff = () => { 
-    stafflist.length = 0;
-    stafflist.push("None")
-    connection.query('Select id, first_name. last_name FROM employee', (err, res) => { 
-        if(err) throw err;
-        stafflist.splice(1); 
-        for(i=0; i<res.length; i++){ 
-            stafflist.push(res[i].first_name.concat(' ', res[i].last_name))
-        }
-        return stafflist;
-    })
 
-    } 
+            ////
+   
+            const taskList = ["Exit application", "Add Employee", "Add role", "Add Department","View all Employee's", "Explore all Employee roles", 'Explore all Departments', 'Update Employee']
+   
+            function startPrompt() { 
+                inquirer.prompt([ 
+                    
 
-                            //gathers all roles for role array 
+                    { 
+                        type:"list",
+                        message: "Welcome to employee tracker,Pick a option from the list below to get started",
+                        name: "choice",
+                        choices: taskList
+ 
+                    }
+                ]).then(function(val) { 
+                    switch(val.choices) { 
+                        case "View all Employee's?": 
+                        viewAllEmployees();
+                        break; 
 
-    const getRole = () => {  
-        RoleList.length = 0; 
-        connection.query('Pick title FROM roles ', (err,res) => { 
-            if(err) throw err; 
-            RoleList.splice(1); for(i=0; i<res.length; i++){ 
-                RoleList.push(res[i].title)
-            }    
-            return RoleList;
-          })
-    }
+                        case "Explore all Employee roles?":
+                        ExpAllRoles();  
+                        break;  
 
-    const getDepartment = () => { 
-        connection.query('Pick dep_name FROM department ', (err,res) => { 
-            if(err) throw err; 
-            DepList.splice(1); for(i=0; i<res.length; i++){ 
-                DepList.push(res[i].dep_name)
-            }    
-         
-          })
-    }
+                        case "Explore all Departments":
+                            ExpAllDep()
+                            break;
+                        
+                        case "Add Employee?": 
+                         addEmployee();
+                         break; 
+                        
+                        case "Update Employee":
+                          updateEmployee();
+                          break; 
+
+                        case "Add Role?":
+                            addRole(); 
+                            break; 
+
+                            case "add Department?":
+                                addDepartment();
+                                break;
+                    }
+
+                })
+            }
+
 
                                //adding staff section\\\\\\\\\\\
 
-    const addStaff = () => {
-        getStaff()
-        getRole()
-        setTimeout(function() { 
-            inquirer.prompt ([{ 
-                name: 'first_name',
-                message: 'Enter First name of employee:'
-            },{ 
-                name: 'last_name',
-                message: 'Enter Last name of employee:'
-            },{
-            name: 'role',
-            message: 'Enter the role of the employee:',
-            type: 'list', 
-            choices: RoleList,
-        }, {
-            name: 'manager',
-            message: "Enter the employee's  manager",
-            choices: stafflist,
-            type: 'list'
-        }
-         ]).then((result) => { 
-
-            let roleIndex = 0; 
-            for(i=0; i<RoleList.length; i++) { 
-                if(RoleList[i] === result.role ) { 
-                    roleIndex = i+1;
-                }
-            }
+   
 
             let managerIndex = 0; 
             for(i=0; i<stafflist.length; i++) { 
@@ -122,215 +101,39 @@ const getStaff = () => {
 
                        //adds department section\\\\
 
-    const addDepartment = () => { 
-        inquirer.prompt ([ 
-            { 
-                new: 'New_Department', 
-                message: 'Enter name of your New department:'
-            }
-        ]).then((result) => { 
-
-            let query = 'INSERT INTO department SET ?'; 
-            connect.query(query, 
-                
-                { 
- 
-                dep_name: result.new_Department
-            }, 
-             (err, res) => {
-                 if (err) throw err; 
-
-             }
-            ),
-            console.log('added Inputs Successfully', result.new_Department,"department"), 
-            root()
-        }
-        )
-    }
-
+   
 
                  ///add role section\\\\\\\
 
-    const addRole = () => { 
-        getDepartment()
-        setTimeout(function() { 
-         inquirer.prompt ([ 
-          { 
-              name: 'title', 
-              message: 'Enter Role title:'
-          },{
-              name: 'department', 
-              message: "Choose department",
-              type:'list',
-              choices: DepList,
-          },
-          { 
-              name: 'salary',
-              message: 'Enter Salary for the role:'
-          }
-         ]).then((result) => { 
-
-            let depIndex = 0; 
-            for(i=0; i<DepList.length; i++) {
-                if (result.department === depList[0]) {
-                depIndex = null;
-                } else if (result.department === depList[i]) {
-                    depIndex = i;
-                }
-            }
-        let query = 'Insert INTO roles SET';  
-        connection.query(query, 
-            { 
-                title: result.title, 
-                salary:result.salary,
-                department_id:depIndex,
-            },
-            (err, res) => { 
-                if(err) throw err;
-            }
-            
-            
-            ), 
-            console.log(`New role "${results.title}" added!`), 
-            root()
-
-        } 
-            ), 30}); 
-
-    }   
-
+   
                             //See all  Employees section///
-    const ExploreAll = () => {
-        let query = `SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, department.dep_name, employees.manager  `
-        query += `FROM employees `
-        query += `JOIN roles ON employees.role_id=roles.id `
-    
-    
-        query += `LEFT JOIN department ON department.id = roles.department_id; `
-
-        connection.query (query, 
-            
-            (err, res) => {
-  if(err) throw err;
-  console.table(res); 
-  root()
-            
-    }
-    )
-
-    }
+    function ExploreAll(){ 
+       connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+          function(er,res) { 
+              if (err) throw err
+              console.table(res)
+              root()
+          })
+        }
 
                        ////////update employees role  section///////////
 
             
-   const updateEmpRole = () => { 
-       stafflist.length = 0; 
-       getStaff()
-       getRole()
-       setTimeout(function(){
-           inquirer.prompt ([
+  
 
-        {  
-            name: 'staff', 
-            message: "Pick employee,to update role", 
-            choices: stafflist,
-            type: 'list'
-        },  {  
-            name: 'new_role', 
-            message: "Input new role", 
-            choices: RoleList,
-            type: 'list'
-        }
-        ]).then((result) => { 
-            let roleIndex = 0; 
-            for(i=0; i<RoleList.length; i++) {
-                if (RoleList[i] === result.new_role) {
-                roleIndex = i+1;
-                } 
-            }
-           let StaffId = stafflist.indexOf(result.staff);
-           const query = connection.query( 
-            'UPDATE employees SET role_id = ? WHERE id = ?',
-            [ 
-                roleIndex, 
-                StaffId 
-            ],
-            (err, res) => { 
-                if(err) throw err; 
-                console.log('Updated Role successfully!!')
-            },root()
-            ); 
-            });
-        },300);
-
-   };
-
-   const ExploreRoles = () => { 
-       let query = 
-       'Select * FROM roles' 
-       connection.query (query, 
-        (err, res) => { 
-            if(err) throw err; 
-            console.table(res); 
-            root()
-        }
-        
-        )
-   }
-
-   const ExploreDepartments =  () => {
-       let query =  
-       `Select * FROM department`
-       connection.query (query,
-     
-       (err, res) => {
-           if (err) throw err;
-           console.table(res);
-           root()
-       }
-       
-       )
-   }
-   
-   const taskList = ['Exit application', 'Add new staff', 'Add new role', 'Explore new department','Explore all staffs', 'Explore all roles', 'Explore all departments', 'Update staff roles']
-
-   const root = () => {
-    inquirer.prompt ([
-        {
-        name: 'task',
-        message: 'Welcome to :',
-        choices: taskList,
-        type: 'list'
-        }
-    ]).then ((response) => {
-        switch (response.task) {
-            case 'Exit application':
-                connection.end()
-                break;
-            case 'Add new staff':
-                addStaff()
-                break;
-            case 'Explore all staff':
-                ExploreAll()
-                break;
-            case 'Update staff roles':
-                updateEmpRole()
-                break;
-            case 'Add new role':
-                addRole()
-                break;
-            case 'Add new department':
-                addDepartment()
-                break;
-            case 'Explore all roles':
-                ExploreRoles()
-                break;  
-            case 'Explore all departments':
-                ExploreDepartments()
-                break;             
-        }
-    })
-}
-init = () => {
-root();
-}   
+   function ExploreRoles () { 
+       connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+     function(err, res) { 
+         if(err) throw err 
+         console.table(res)
+         root()
+     })
+    }
+   function ExploreDepartments  (){
+       connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", 
+      function (err, res) {  
+          if (err) throw err
+          console.table(res)
+          root()
+      })
+      
--- schema.sql
+++ schema.sql
@@ -1,25 +1,79 @@
-DROP DATABASE employee_db;
+DROP DATABASE IF EXISTS employee_DB;
+
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employees (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR (255) NOT NULL,
-    last_name VARCHAR (255) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    manager_name VARCHAR (255)
-);
-
-CREATE TABLE roles (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR (255),
-    salary INTEGER NOT NULL,
-    department_id INTEGER NOT NULL
-);
-
+-- DEPARTMENT TABLE ----
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    dep_name VARCHAR (255)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
 );
+-- DEPARTMENT TABLE ----
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+-- EMPLOYEE ROLE TABLE ----
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
+
+-- DEPARTMENT SEEDS -----
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
+
+-- EMPLOYEE ROLE SEEDS -------
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
+
+-- EMPLOYEE SEEDS -------
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Eric", "Cartmen", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Oliva", "Pope", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Sakuragi","Hanamichi",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jojo", "Bizzare", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Daenerys ", "Targaryen", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Cersei", "Lannister", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Peta", "Parka", 2, 7);
+
+-- SELECTING FOR CREATING 
+-- TABLES IN OUR SQL WORKBENCH 
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
